5aa66d0980d41ef3de9cfa35805e6e15960cfba2
dudu <complex857@gmail.com>
2014-08-21 15:27:54 +0200

    whitepsace cleanups

--- phonegap.facebook.inappbrowser.js
+++ phonegap.facebook.inappbrowser.js
@@ -49,10 +49,10 @@
          * @return {Boolean}
          */
         exists: function(test, type) {
-            if(typeof type !== 'undefined') {
-                if((typeof test !== 'undefined' && test !== '') && typeof test === type) return true;
+            if (typeof type !== 'undefined') {
+                if ((typeof test !== 'undefined' && test !== '') && typeof test === type) return true;
             } else {
-                if(typeof test !== 'undefined' && test !== '') return true;
+                if (typeof test !== 'undefined' && test !== '') return true;
             }
             return false;
         },
@@ -65,18 +65,18 @@
          * @param  {Object}   data     Data to send
          */
         ajax: function(type, url, callback, data) {
-            if(!FacebookInAppBrowser.exists(type) || !FacebookInAppBrowser.exists(url) || !FacebookInAppBrowser.exists(callback)) {
+            if (!FacebookInAppBrowser.exists(type) || !FacebookInAppBrowser.exists(url) || !FacebookInAppBrowser.exists(callback)) {
                 console.log('[FacebookInAppBrowser] type, url and callback parameters are necessary.');
                 return false;
             }
-            if(!FacebookInAppBrowser.exists(callback, 'function')) {
+            if (!FacebookInAppBrowser.exists(callback, 'function')) {
                 console.log('[FacebookInAppBrowser] callback must be a function.');
                 return false;
             }
 
             var request = new XMLHttpRequest();
             request.open(type, url, true);
-            if(data) {
+            if (data) {
                 request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
                 request.setRequestHeader("Content-length", data.length);
                 request.setRequestHeader("Connection", "close");
@@ -86,7 +86,7 @@
                 if (request.readyState == 4) {
                     if (request.status == 200 || request.status === 0) {
                         var data = request.responseText;
-                        if(data) {
+                        if (data) {
                             callback(data);
                         } else {
                             callback(false);
@@ -121,7 +121,7 @@
          */
         login: function(data) {
 
-            if(!FacebookInAppBrowser.exists(this.settings.appId) || !FacebookInAppBrowser.exists(this.settings.redirectUrl)) {
+            if (!FacebookInAppBrowser.exists(this.settings.appId) || !FacebookInAppBrowser.exists(this.settings.redirectUrl)) {
                 console.log('[FacebookInAppBrowser] You need to set up your app id and redirect url.');
                 return false;
             }
@@ -133,11 +133,11 @@
             authorize_url += "&response_type=token";
             authorize_url += "&type=user_agent";
 
-            if(FacebookInAppBrowser.exists(this.settings.permissions)) {
+            if (FacebookInAppBrowser.exists(this.settings.permissions)) {
                 authorize_url += "&scope=" + this.settings.permissions;
             }
 
-            if(FacebookInAppBrowser.exists(data.send, 'function')) data.send();
+            if (FacebookInAppBrowser.exists(data.send, 'function')) data.send();
 
             var faceView,
                 userDenied = false;
@@ -151,7 +151,7 @@
 
                 timeoutOccurred = true;
 
-                if(FacebookInAppBrowser.exists(data.timeout, 'function')) {
+                if (FacebookInAppBrowser.exists(data.timeout, 'function')) {
                     setTimeout(function() {
                         data.timeout();
                     }, 0);
@@ -170,7 +170,7 @@
                     window.localStorage.setItem('facebookAccessToken', access_token);
                     faceView.close();
 
-                    if(FacebookInAppBrowser.exists(data.success, 'function')) {
+                    if (FacebookInAppBrowser.exists(data.success, 'function')) {
                         setTimeout(function() {
                             data.success(access_token);
                         }, 0);
@@ -181,7 +181,7 @@
                 // User denied
                 if (location.url.indexOf("error_reason=user_denied") !== -1) {
                     userDenied = true;
-                    if(FacebookInAppBrowser.exists(data.denied, 'function')) {
+                    if (FacebookInAppBrowser.exists(data.denied, 'function')) {
                         setTimeout(function() {
                             data.denied();
                         }, 0);
@@ -193,7 +193,7 @@
 
             // On finished loading
             faceView.addEventListener('loadstop', function(){
-                if(timeoutOccurred == true) return;
+                if (timeoutOccurred == true) return;
 
                 clearTimeout(timeout); // Clear the onTimeout function
                 faceView.show();
@@ -202,23 +202,23 @@
             // On exit
             faceView.addEventListener('exit', function() {
 
-                if(window.localStorage.getItem('facebookAccessToken') === null && userDenied === false) {
+                if (window.localStorage.getItem('facebookAccessToken') === null && userDenied === false) {
                     // InAppBrowser was closed and we don't have an app id
-                    if(FacebookInAppBrowser.exists(data.complete, 'function')) {
+                    if (FacebookInAppBrowser.exists(data.complete, 'function')) {
                         setTimeout(function() {
                             data.complete(false);
                         }, 0);
                     }
 
-                } else if(userDenied === false) {
-                    if(FacebookInAppBrowser.exists(data.complete, 'function')) {
+                } else if (userDenied === false) {
+                    if (FacebookInAppBrowser.exists(data.complete, 'function')) {
                         setTimeout(function() {
                             data.complete(window.localStorage.getItem('facebookAccessToken'));
                         }, 0);
                     }
                 }
 
-                if(window.localStorage.getItem('facebookAccessToken') !== null) {
+                if (window.localStorage.getItem('facebookAccessToken') !== null) {
                     setTimeout(function() {
                         var userInfoCallback = FacebookInAppBrowser.exists(data.userInfo, 'function') ? data.userInfo : undefined;
                         FacebookInAppBrowser.getInfo(userInfoCallback);
@@ -236,7 +236,7 @@
          * @param  {Function} afterCallback Success/Error callback
          */
         getInfo: function(afterCallback) {
-            if(window.localStorage.getItem('facebookAccessToken') === null) {
+            if (window.localStorage.getItem('facebookAccessToken') === null) {
                 console.log('[FacebookInAppBrowser] No facebookAccessToken. Try login() first.');
                 return false;
             }
@@ -245,17 +245,17 @@
             console.log('[FacebookInAppBrowser] getInfo request url: ' + get_url);
 
             FacebookInAppBrowser.ajax('GET', get_url, function(data) {
-                if(data) {
+                if (data) {
                     var response = JSON.parse(data);
                     console.log("[FacebookInAppBrowser] User id: " + response.id);
-                    if(FacebookInAppBrowser.exists(response.id)) window.localStorage.setItem('uid', response.id);
-                    if(FacebookInAppBrowser.exists(afterCallback, 'function')) {
+                    if (FacebookInAppBrowser.exists(response.id)) window.localStorage.setItem('uid', response.id);
+                    if (FacebookInAppBrowser.exists(afterCallback, 'function')) {
                         setTimeout(function() {
                             afterCallback(response);
                         }, 0);
                     }
                 } else {
-                    if(FacebookInAppBrowser.exists(afterCallback, 'function')) {
+                    if (FacebookInAppBrowser.exists(afterCallback, 'function')) {
                         setTimeout(function() {
                             afterCallback(false);
                         }, 0);
@@ -271,7 +271,7 @@
          * @param  {Function} afterCallback Success/Error callback
          */
         getPermissions: function(afterCallback) {
-            if(window.localStorage.getItem('uid') === null) {
+            if (window.localStorage.getItem('uid') === null) {
                 console.log('[FacebookInAppBrowser] No user id. Try getInfo() first.');
                 return false;
             }
@@ -282,15 +282,15 @@
             console.log('[FacebookInAppBrowser] getPermissions request url: ' + get_url);
 
             FacebookInAppBrowser.ajax('GET', get_url, function(data) {
-                if(data) {
+                if (data) {
                     var response = JSON.parse(data);
-                    if(response.data[0]) permissions = response.data[0];
+                    if (response.data[0]) permissions = response.data[0];
                     console.log("[FacebookInAppBrowser] Permissions: " + JSON.stringify(permissions));
-                    if(FacebookInAppBrowser.exists(afterCallback, 'function')) {
+                    if (FacebookInAppBrowser.exists(afterCallback, 'function')) {
                         afterCallback(permissions);
                     }
                 } else {
-                    if(FacebookInAppBrowser.exists(afterCallback, 'function')) {
+                    if (FacebookInAppBrowser.exists(afterCallback, 'function')) {
                         afterCallback(false);
                     }
                 }
@@ -305,7 +305,7 @@
          * @param  {Function} afterCallback   Success/Error callback
          */
         post: function(data, afterCallback) {
-            if(!FacebookInAppBrowser.exists(data.name) ||
+            if (!FacebookInAppBrowser.exists(data.name) ||
                 !FacebookInAppBrowser.exists(data.link) ||
                 !FacebookInAppBrowser.exists(data.description) ||
                 !FacebookInAppBrowser.exists(data.picture) ||
@@ -313,7 +313,7 @@
                 console.log('[FacebookInAppBrowser] name, link, description, picture and message are necessary.');
                 return false;
             }
-            if(!FacebookInAppBrowser.exists(FacebookInAppBrowser.settings.appId) || !FacebookInAppBrowser.exists(window.localStorage.getItem('facebookAccessToken')) || window.localStorage.getItem('facebookAccessToken') === null) {
+            if (!FacebookInAppBrowser.exists(FacebookInAppBrowser.settings.appId) || !FacebookInAppBrowser.exists(window.localStorage.getItem('facebookAccessToken')) || window.localStorage.getItem('facebookAccessToken') === null) {
                 console.log('[FacebookInAppBrowser] You need to set your app id in FacebookInAppBrowser.settings.appId and have a facebookAccessToken (try login first)');
                 return false;
             }
@@ -323,19 +323,19 @@
                     '&name='+data.name+'&link='+data.link+'&description='+data.description+'&picture='+data.picture+'&message='+data.message;
 
             FacebookInAppBrowser.ajax('POST', post_url, function(data) {
-                if(data) {
+                if (data) {
                     var response = JSON.parse(data);
-                    if(response.id) {
-                        if(FacebookInAppBrowser.exists(afterCallback, 'function')) {
+                    if (response.id) {
+                        if (FacebookInAppBrowser.exists(afterCallback, 'function')) {
                             afterCallback(response.id);
                         }
                     } else {
-                        if(FacebookInAppBrowser.exists(afterCallback, 'function')) {
+                        if (FacebookInAppBrowser.exists(afterCallback, 'function')) {
                             afterCallback(false);
                         }
                     }
                 } else {
-                    if(FacebookInAppBrowser.exists(afterCallback, 'function')) {
+                    if (FacebookInAppBrowser.exists(afterCallback, 'function')) {
                         afterCallback(false);
                     }
                 }
@@ -348,7 +348,7 @@
          * @param  {Function} afterCallback Success/Error callback
          */
         invite: function(inviteText, afterCallback) {
-            if(typeof inviteText === 'undefined') {
+            if (typeof inviteText === 'undefined') {
                 console.log('[FacebookInAppBrowser] inviteText is a required parameter.');
                 return false;
             }
@@ -369,30 +369,30 @@
                 callback = function(location) {
                     console.log("[FacebookInAppBrowser] Event 'loadstart': " + JSON.stringify(location));
 
-                    if(location.url == request_url) {
+                    if (location.url == request_url) {
                         // Do nothing
 
                     } else if (location.url.indexOf("?request=") !== -1) {
                         // Success
                         faceView.close();
 
-                        if(FacebookInAppBrowser.exists(afterCallback, 'function')) {
+                        if (FacebookInAppBrowser.exists(afterCallback, 'function')) {
                             setTimeout(function() {
                                 afterCallback(true);
                             }, 0);
                         }
 
-                    } else if(location.url.indexOf('error_code=') !== -1) {
+                    } else if (location.url.indexOf('error_code=') !== -1) {
                         // Error
                         faceView.close();
 
-                        if(FacebookInAppBrowser.exists(afterCallback, 'function')) {
+                        if (FacebookInAppBrowser.exists(afterCallback, 'function')) {
                             setTimeout(function() {
                                 afterCallback(false);
                             }, 0);
                         }
 
-                    } else if(location.url === obj.settings.redirectUrl + '#_=_') {
+                    } else if (location.url === obj.settings.redirectUrl + '#_=_') {
                         // User clicked Cancel
                         face.close();
 
@@ -416,13 +416,13 @@
                 callback = function(location) {
                     console.log("[FacebookInAppBrowser] Event 'loadstart': " + JSON.stringify(location));
 
-                    if(location.url == logout_url) {
+                    if (location.url == logout_url) {
                         // Do nothing
 
                     } else if(location.url ===  FacebookInAppBrowser.settings.redirectUrl + '#_=_' || location.url === FacebookInAppBrowser.settings.redirectUrl || location.url === FacebookInAppBrowser.settings.redirectUrl + '/') {
                         face.close();
 
-                        if(FacebookInAppBrowser.exists(afterCallback, 'function')) {
+                        if (FacebookInAppBrowser.exists(afterCallback, 'function')) {
                             setTimeout(function() {
                                 afterCallback();
                             }, 0);
